ab83ed9145a7f92c3d4e5ca9c1797f171dc5f630
ricomaulana510@gmail.com <ricomaulana510@gmail.com>
2022-01-10 10:51:20 +0700

    Tugas 2 : base admin template

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout.master');
 });
+
+Route::get('/content', function () {
+    return view('content');
+});
+
